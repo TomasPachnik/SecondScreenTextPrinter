bd0048fd375d91237e6b6703693dc364f856268b
Tomas Pachnik <tomas.pachnik@gmail.com>
2017-04-24 19:38:03 +0200

    Default gui window

---
+++ src/main/java/sk/tomas/sstp/gui/ControlPanel.java
@@ -0,0 +1,7 @@
+package sk.tomas.sstp.gui;
+
+/**
+ * Created by tomas on 4/24/17.
+ */
+public class ControlPanel {
+}
---
+++ src/main/java/sk/tomas/sstp/main/Main.java
@@ -0,0 +1,12 @@
+package sk.tomas.sstp;
+
+/**
+ * Created by tomas on 4/24/17.
+ */
+public class Main {
+
+    public static void main (String [] args){
+        System.out.println("Hello world");
+    }
+
+}
